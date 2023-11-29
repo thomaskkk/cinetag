3eca2ba67b988981ef56acfda17e0631e9887f17
Thomas Kuryura <thomas.adm@gmail.com>
2023-11-29 15:43:49 -0300

    Add base React

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <h1>Olá mundo</h1>
+  </React.StrictMode>
+);
