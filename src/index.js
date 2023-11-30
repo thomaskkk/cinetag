a3d5b16c1f5c52d197913e4e4c98bedb530ebb4b
Thomas Kuryura <thomas.kuryura@idwall.co>
2023-11-30 08:57:40 -0300

    Adding routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import AppRoutes from "routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <h1>Olá mundo</h1>
+    <AppRoutes />
   </React.StrictMode>
 );
---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,7 @@
+import "./Inicio.css";
+
+const Inicio = () => {
+  return <h1>Olá mundo com rotas</h1>;
+};
+
+export default Inicio;
