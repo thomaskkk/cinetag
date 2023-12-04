996f80fae528f3cfda6c2ea922ffab69709ac18b
Thomas Kuryura <thomas.kuryura@idwall.co>
2023-12-04 14:04:29 -0300

    Finishing favorite logic

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -2,9 +2,10 @@ import Banner from "components/Banner";
 import Title from "components/Title";
 import Card from "components/Card";
 import styles from "./Favorites.module.css";
-import videos from "json/db.json";
+import { useFavoriteContext } from "context/Favorites";
 
 const Favorites = () => {
+  const { favorite } = useFavoriteContext();
   return (
     <>
       <Banner imagem="favoritos" />
@@ -12,8 +13,8 @@ const Favorites = () => {
         <h1>Meus Favoritos</h1>
       </Title>
       <section className={styles.container}>
-        {videos.map((video) => (
-          <Card {...video} key={video.id} />
+        {favorite.map((fav) => (
+          <Card {...fav} key={fav.id} />
         ))}
       </section>
     </>
